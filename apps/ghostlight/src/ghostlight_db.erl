296ed8d967a92a4bf50f10df0c049bb4de5156f1
Pablo Meier <pablo.a.meier@gmail.com>
2015-03-23 16:43:40 -0400

    alright alrigtht orgs done

--- apps/ghostlight/src/ghostlight_db.erl
+++ apps/ghostlight/src/ghostlight_db.erl
@@ -1,35 +1,49 @@
 %%% The DATABASE
 %%%
 %%% The primary value-prop of Ghostlight comes from its database. Virtually
-%%% _everything_ that gives it value over a pile of programs comes from the
+%%% _everything_ that gives it value over a pile of playbills comes from the
 %%% fact that a) we have a well-maintained, well-run database containing all
-%%% the information, and b) that the data is good.
+%%% the information, and b) that data is easily searchable, well-presented,
+%%% and c) that the data is Good.
 %%%
-%%% While b) is mostly a policy issue (and some data science I don't know),
-%%% most of a) gets handled here!
+%%% While c) is mostly a policy issue (and some data science I don't know),
+%%% and b) is the interface to the app itself, most of a) gets handled here!
 %%%
 %%% Here is the high-level on the database:
 %%% * It exposes a number of API functions to the app that are mostly wrappers
 %%%   to gen_server:call/cast. Technically speaking, we can change everything
 %%%   about this module EXCEPT this -- we could change DB engines, ditch
 %%%   gen_server, whatever. Highly unlikely, but that's the hard module
-%%%   boundary.
+%%%   boundary for the rest of the app.
 %%% * We make it a gen_server for the well-known obvious reasons: we can restart
 %%%   it, supervise it, etc., and it's a solid way to maintain the state that
 %%%   we'll need (parsed queries, PSQL connections).
-%%%
-%%% Since the data is so spread out with a myriad of hairy relationships, 
-%%% generating the exact queries for the use case requires something of a 
-%%% calculus.
-%%%
-%%% Technically speaking, that's what's 
-%%%
+%%% * We use Postgres because it's actually Free Free, unlike Neo4j, relies on
+%%%   SQL which we and the world know, and is rock-rock-steady.
 %%%
 %%% Some ground rules for the module:
 %%%
 %%% * SQL is invisible. No evidence of the statements/queries or the rows and
-%%%   row structure should escape here. We trade entirely in the records
-%%%   specified in ghostlight_data.
+%%%   row structure should escape this module. We trade entirely in the records
+%%%   specified in ghostlight_data. Sometimes they're the same as input records,
+%%%   sometimes they they're <datatype>_return for when we couple together
+%%%   other data during a query.
+%%%
+%%% # Inserting New Data
+%%%
+%%% Since the data is so spread out with a myriad of hairy foreign key
+%%% relationships, generating the exact queries for the use case requires something
+%%% of a calculus. Virtually all data types have a get_<type>_inserts which takes a
+%%% record from `ghostlight_data` and returns two-tuple -> a list of inserts, and
+%%% the newly-generated ID of the entity being created. So:
+%%%
+%%% get_show_inserts(#show{}) -> {[Inserts], ShowId}
+%%%   Insert :: {PreparedStmt, [Arg]}
+%%%   ShowId :: UUID
+%%%
+%%% Here's where it gets interesting: given than a show contains performances, _it_
+%%% will call get_performance_inserts, and bundle THOSE inserts into its own. So it
+%%% really becomes something like Russian Nesting Dolls.
 %%%
 -module(ghostlight_db).
 -behaviour(gen_server).
@@ -99,7 +113,7 @@ handle_call({insert_show, Show}, _From, State) ->
     Reply = exec_batch(Inserts, State),
     {reply, Reply, State};
 
-%% Like insertions, getting a show is a pretty hairy endeavor since it touches
+%% As with insertions, querying a show is a pretty hairy endeavor since it touches
 %% so many tables. There's probably some major SQL-foo I could employ to get 
 %% almost exactly what I need.
 %%
